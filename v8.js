854245ed2a43e7363756cb15f7b9ff40f04a6c41
Luke Snowden <luke@purposemedia.co.uk>
2014-10-23 23:08:45 +0100

    initial commit
    http://luke.sno.wden.co.uk/v8 for more info

---
+++ v8.js
@@ -0,0 +1,164 @@
+
+/*!
+* 	V8 - Event Frequency API
+* 	https://github.com/lukesnowden/v8
+* 	http://luke.sno.wden.co.uk/v8
+* 	Copyright 2014 Luke Snowden
+* 	Released under the MIT license:
+* 	http://www.opensource.org/licenses/mit-license.php
+*/
+
+;( function ( $ ) {
+
+	$.fn.v8 = function (opts)
+	{
+		var opts = $.extend({
+
+			maxFrequency : 10,
+
+			milliseconds : 1000,
+
+			updateFrequency : 100,
+
+			keyBind : [],
+
+			preventClickAndTouch : false
+
+		}, opts);
+
+		var touchDevice = 'touchstart' in window;
+
+		var touchstart = touchDevice ? 'touchstart' : 'mousedown';
+
+		var touchend = touchDevice ? 'touchend' : 'mouseup';
+
+		var listener = function( _elm, _namespace, _callback ) {
+		    var touching = false;
+		    var count = 0;
+		    function bind( namespace, callback ) {
+		        _elm.on( touchstart + '.' + namespace, function(e){
+		            if( ! touching ) {
+		                touching = true;
+		                count++;
+		                callback( count );
+		                _elm.off( touchstart + '.' + namespace ).on( touchend + '.' + namespace, function(e){
+		                    touching = false;
+		                    _elm.off( touchend + '.' + namespace );
+		                    bind( namespace, callback );
+		                });
+		            } else {
+		                e.preventDefault();
+		                return false;
+		            }
+		        });
+		    }
+		    bind( _namespace, _callback );
+		};
+
+		/**
+		 * [init description]
+		 * @param  {[type]} elm [description]
+		 * @return {[type]}     [description]
+		 */
+
+		var init = function( elm ) {
+
+			var that = elm;
+
+			var obj = $( that );
+
+			var timeTotal = 0;
+
+			var clickTimes = [];
+
+			var spark = null;
+
+			var highest = 0;
+
+			var numberOfMilliseconds = opts.milliseconds;
+
+			var start = function() {
+
+				var currentTime = ( new Date() ).getTime();
+
+				timeTotal += currentTime;
+
+				clickTimes.push( currentTime );
+
+				if( spark === null ) {
+
+					spark = setInterval( function() {
+
+						if( numberOfMilliseconds === opts.milliseconds && clickTimes.length === 0 ) {
+
+							clearInterval( spark );
+							spark = null;
+							timeTotal = 0;
+							clickTimes = [];
+
+						} else {
+
+							var currentTime = ( new Date() ).getTime();
+
+							while( clickTimes.length && ( currentTime - clickTimes[0] > opts.milliseconds ) ) {
+								timeTotal -= clickTimes.shift();
+							}
+
+							if ( clickTimes.length > 0 ) {
+								numberOfMilliseconds = clickTimes[clickTimes.length - 1] - clickTimes[0];
+							} else {
+								numberOfMilliseconds = opts.milliseconds;
+							}
+
+							var ev = $.Event( 'tick.v8' );
+							ev.milliseconds = numberOfMilliseconds;
+							ev.clickCount = clickTimes.length;
+							ev.updateFrequency = opts.updateFrequency;
+							ev.targetPercentage = ( clickTimes.length * 100 / opts.maxFrequency ).toFixed(0);
+
+							if( ev.targetPercentage > highest ) {
+								highest = ev.targetPercentage;
+							}
+
+							ev.highestPercentage = highest;
+
+							if( ev.targetPercentage > 100 ) {
+								ev.targetPercentage = 100;
+							}
+							obj.trigger( ev );
+
+						}
+
+					}, opts.updateFrequency );
+
+				}
+
+			}
+
+			if( ! opts.preventClickAndTouch ) {
+				new listener( obj, 'v8', start );
+			}
+
+			if( opts.keyBind.length !== 0 ) {
+				obj.keyup( function( e ) {
+					if( $.inArray( e.which, opts.keyBind ) !== -1 ) {
+						start();
+					}
+				});
+			}
+
+		};
+
+		/**
+		 * [description]
+		 * @param  {[type]} i [description]
+		 * @return {[type]}   [description]
+		 */
+
+		return $(this).each( function ( i ) {
+			init( this );
+		});
+
+	};
+
+})( jQuery );
